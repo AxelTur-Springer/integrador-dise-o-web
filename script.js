8fea27ab1a7880ce4fcd4dc3486bf5026dc09c83
AxelTur-Springer <axel.turspringer@gmail.com>
2022-01-27 21:23:47 -0300

    agrandando nav y agregando cosas de full size

--- script.js
+++ script.js
@@ -13,3 +13,12 @@ function myFunction() {
 
 }
 
+
+const navbar = document.querySelector('.nav-bar');
+window.onscroll = () => {
+    if (window.scrollY > 0) {
+        navbar.classList.add('nav-active');
+    } else {
+        navbar.classList.remove('nav-active');
+    }
+};
