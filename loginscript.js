bbcdfca62c3badd64bcb0ea3a057c1da0d64e1a1
AxelTur-Springer <axel.turspringer@gmail.com>
2022-02-03 19:48:24 -0300

    adding animacion

--- loginscript.js
+++ loginscript.js
@@ -4,16 +4,19 @@ function myFunction(){
 const nameAppear = document.getElementById("nametag")
 const submitbtn = document.getElementById("submitbtn") 
 const botonReg = document.getElementById("botonReg") 
+const slider = document.getElementById("division2") 
 if (nameAppear.style.display=="none"){
-    nameAppear.style.display="flex";
+   nameAppear.style.display="flex";
     submitbtn.value="Registrate"
-    botonReg.innerHTML="Tenes una cuenta ? Inicia Sesion !"
+    botonReg.childNodes[0].textContent = "Tenes una cuenta? Inicia Sesion!"
+    slider.style.animation="prueba 2s 0s forwards"
     }
     else{
-        nameAppear.style.display="none";
+       slider.style.height="inital"
+     nameAppear.style.display="none";
         submitbtn.value="Ingresar"
-        
-        botonReg.innerHTML="No tenes una cuenta ? Registrate !"
+        slider.style.animation="none"
+        botonReg.childNodes[0].textContent = "No tenes una cuenta? Registrate!"
     }
 
 
