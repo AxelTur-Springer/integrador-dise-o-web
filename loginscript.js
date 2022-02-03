be5e3f460c73de3ac83c533e16c6acdb7e7f058b
AxelTur-Springer <axel.turspringer@gmail.com>
2022-02-03 20:18:51 -0300

    finished login

--- loginscript.js
+++ loginscript.js
@@ -1,21 +1,24 @@
-
+alert("Cuando ingrese pruebe la funcionalidad del ultimo boton que pregunta su tenes una cuenta graciaas")
 
 function myFunction(){ 
 const nameAppear = document.getElementById("nametag")
 const submitbtn = document.getElementById("submitbtn") 
 const botonReg = document.getElementById("botonReg") 
 const slider = document.getElementById("division2") 
+const logReg = document.getElementById("logReg") 
 if (nameAppear.style.display=="none"){
    nameAppear.style.display="flex";
     submitbtn.value="Registrate"
     botonReg.childNodes[0].textContent = "Tenes una cuenta? Inicia Sesion!"
     slider.style.animation="prueba 2s 0s forwards"
+    logReg.childNodes[0].textContent ="Registrate"
     }
     else{
        slider.style.height="inital"
      nameAppear.style.display="none";
         submitbtn.value="Ingresar"
         slider.style.animation="none"
+        logReg.childNodes[0].textContent ="Inicia Session"
         botonReg.childNodes[0].textContent = "No tenes una cuenta? Registrate!"
     }
 
